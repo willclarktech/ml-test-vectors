f0a497b1b78de31fff70e0874cd7297612ef07e9
willclarktech <willclarktech@users.noreply.github.com>
2023-01-10 21:30:37 +0100

    Add some utils

---
+++ ml_test_vectors/utils.py
@@ -0,0 +1,22 @@
+import os
+from functools import partial
+from typing import List
+
+import numpy as np
+import torch
+
+from ml_test_vectors.types import Tensor
+
+
+def get_test_vector_file_path(current_file_path: str) -> str:
+    dirname = os.path.dirname(os.path.realpath(current_file_path))
+    return f"{dirname}/test_vector.json"
+
+
+def detorch(tensors: List[torch.Tensor]) -> Tensor:
+    return [tensor.tolist() for tensor in tensors]
+
+
+assert_allclose = partial(
+    np.testing.assert_allclose, atol=1.0e-7, rtol=1.0e-7, equal_nan=True
+)
