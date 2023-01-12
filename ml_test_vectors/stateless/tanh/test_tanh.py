b0ac4752988ef5227ecbffda2818484c378460fb
willclarktech <willclarktech@users.noreply.github.com>
2023-01-12 15:35:14 +0100

    Rearrange tests to use pytest

---
+++ ml_test_vectors/stateless/tanh/test_tanh.py
@@ -0,0 +1,13 @@
+from ml_test_vectors.stateless.tanh.implementation import backward, forward
+from ml_test_vectors.stateless.testutils import check_backward, check_forward
+from ml_test_vectors.utils import get_test_vector_file_path
+
+test_vector_file_path = get_test_vector_file_path(__file__)
+
+
+def test_forward():
+    check_forward(test_vector_file_path, forward)
+
+
+def test_backward():
+    check_backward(test_vector_file_path, backward)
---
+++ ml_test_vectors/stateless/testutils.py
@@ -0,0 +1,38 @@
+from ml_test_vectors.types import (
+    StatelessDerivativeFunction,
+    StatelessFunction,
+    test_vector_from_json,
+)
+from ml_test_vectors.utils import assert_allclose
+
+
+def check_forward(
+    test_vector_file_path: str,
+    forward: StatelessFunction,
+) -> None:
+    with open(test_vector_file_path, "r", encoding="utf8") as file:
+        test_vector = test_vector_from_json(file.read())
+        for inp, expected in zip(test_vector.inputs, test_vector.outputs):
+            output = forward(inp)
+            assert_allclose(
+                output,
+                expected,
+                err_msg=f"Forward input was {inp}",
+            )
+
+
+def check_backward(
+    test_vector_file_path: str,
+    backward: StatelessDerivativeFunction,
+) -> None:
+    with open(test_vector_file_path, "r", encoding="utf8") as file:
+        test_vector = test_vector_from_json(file.read())
+        for inp, output, expected in zip(
+            test_vector.inputs, test_vector.outputs, test_vector.gradients
+        ):
+            gradient = backward(inp, output)
+            assert_allclose(
+                gradient,
+                expected,
+                err_msg=f"Backward input was {inp}; output was {output}",
+            )
