79c7bcd20f94222b9bb525e12e0623790c25b2fe
willclarktech <willclarktech@users.noreply.github.com>
2023-01-13 16:03:45 +0100

    Add comment to TS Tensor type

--- ml_test_vectors/types.ts
+++ ml_test_vectors/types.ts
@@ -1,4 +1,10 @@
 export type Scalar = number;
+// TODO: Define more precisely with max dimensions?
+// eg
+// Tensor0 = Scalar
+// Tensor1 = readonly Tensor0[]
+// Tensor2 = readonly Tensor1[]
+// Tensor = Tensor0 | Tensor1 | Tensor2...
 export type Tensor = Scalar | readonly Tensor[];
 
 export interface TestVector {
