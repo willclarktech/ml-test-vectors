58f7624f10b0fcfdf5246e73dedfbc5ae7d39f8a
willclarktech <willclarktech@users.noreply.github.com>
2023-01-13 15:15:37 +0100

    Add TS stateless testutils

---
+++ ml_test_vectors/stateless/testutils.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+import type {
+	StatelessDerivativeFunction,
+	StatelessFunction,
+	TestVector,
+} from "../types";
+
+export const loadTestVector = (filePath: string): TestVector => {
+	const contents = fs.readFileSync(filePath, "utf8");
+	// TODO: Validate shape
+	return JSON.parse(contents) as TestVector;
+};
+
+export const checkForward = (filePath: string, fn: StatelessFunction): void => {
+	const testVector = loadTestVector(filePath);
+	testVector.inputs.forEach((input, i) => {
+		expect(fn(input)).toBeCloseTo(testVector.outputs[i]);
+	});
+};
+
+export const checkBackward = (
+	filePath: string,
+	fn: StatelessDerivativeFunction,
+): void => {
+	const testVector = loadTestVector(filePath);
+	testVector.inputs.forEach((input, i) => {
+		expect(fn(input, testVector.outputs[i])).toBeCloseTo(
+			testVector.gradients[i],
+		);
+	});
+};
