0136eafd5b1ab6e5437328592bfd2fe07ce3f538
willclarktech <willclarktech@users.noreply.github.com>
2023-01-16 12:57:46 +0100

    Explicitly enumerate Tensor types

--- ml_test_vectors/core.py
+++ ml_test_vectors/core.py
@@ -2,12 +2,16 @@ from typing import Callable, List, Optional, TypedDict, Union
 
 import torch
 
+# TODO: Figure out if dimensions can be parameterized in mypy so we can escape a maximum
 Scalar = float
 Vector = List[float]
-Tensor = Union[Scalar, List["Tensor"]]
+Matrix = List[Vector]
+Tensor4 = List[Matrix]
+Tensor5 = List[Tensor4]
+Tensor = Union[Scalar, Vector, Matrix, Tensor4, Tensor5]
 
 StatelessFunction = Callable[[Tensor], Tensor]
-StatelessDerivativeFunction = Callable[[Tensor, Tensor], Tensor]
+StatelessDerivativeFunction = Callable[[Tensor, Optional[Tensor]], Tensor]
 TorchStatelessFunction = Callable[[torch.Tensor], torch.Tensor]
 
 
