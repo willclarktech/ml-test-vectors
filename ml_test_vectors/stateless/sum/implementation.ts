378aa5f46feb7122ccaef6f58175f29a606225dc
willclarktech <willclarktech@users.noreply.github.com>
2023-01-16 12:27:32 +0100

    Tidy sum implementation

--- ml_test_vectors/stateless/sum/implementation.ts
+++ ml_test_vectors/stateless/sum/implementation.ts
@@ -6,11 +6,9 @@ import type {
 } from "../../core";
 import { isScalar, onesLike } from "../../utils";
 
-export const forward: StatelessFunction<Tensor, Scalar> = (input) => {
-	if (isScalar(input)) {
-		return input;
-	}
-	return input.reduce<number>((subtotal, n) => subtotal + forward(n), 0);
-};
+export const forward: StatelessFunction<Tensor, Scalar> = (input) =>
+	isScalar(input)
+		? input
+		: input.reduce<number>((subtotal, n) => subtotal + forward(n), 0);
 
 export const backward: StatelessDerivativeFunction = (input) => onesLike(input);
