bdf320b33783dc8e3a859caa2c401811462a715e
willclarktech <willclarktech@users.noreply.github.com>
2023-01-16 12:28:40 +0100

    Add scalar values to stateless default data

--- ml_test_vectors/stateless/default_data.py
+++ ml_test_vectors/stateless/default_data.py
@@ -3,6 +3,15 @@ from typing import List
 from ml_test_vectors.core import Tensor
 
 default_input_data: List[Tensor] = [
+    0.0,
+    -0.1,
+    0.1,
+    -1.0,
+    1.0,
+    -2.0,
+    2.0,
+    -10.0,
+    10.0,
     [0.0],
     [-0.1],
     [0.1],
